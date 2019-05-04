c960fe975492a5bdb75ec834b3245793b6ee85cc
op7418 <op7418@op7418deMacBook-Pro.local>
2019-05-04 17:00:27 +0800

    动画

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
         <h1>Learn to <br />design and code React apps</h1>
         <p>Complete courses about the best tools and design
        systems. Prototype and build apps with React and Swift.</p>
-        <Link to="/page-2/">Go to page 2</Link>
+        <Link to="/page-2/">WATCH THE VIDEO</Link>
         </div>
     </div> 
   </Layout>
